d07be8a1aee785d16acfb5bd46a3f957fbd2c888
Nijar <nijarr2020@gmail.com>
2019-08-14 21:22:24 +0530

    profile added

--- form/src/Login.js
+++ form/src/Login.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react'
 import GoogleSign from 'react-google-login'
+import User from './User';
+
 
 class Login extends Component {
   state = {
-    loggedIn: false,
     email: '',
     name: '',
-    picture: ''
+    picture: '',
+    det: ''
   }
- 
-  
- 
+
   render() {
-    const responseGoogle=(e)=>{
+    const responseGoogle = (e) => {
       console.log(e.profileObj);
+      this.setState({
+        email: e.profileObj.email,
+        name: e.profileObj.name,
+        picture: e.profileObj.ImageUrl,
+        det: e.profileObj
+      })
     }
-   
     return (
       <div className="container center">
-
-        <div className='card-panel' style={{ margin: '3em'}}>
+        <div className='card-panel' style={{ margin: '3em' }}>
           <span>Login with Google</span>
-          <GoogleSign
-            clientId='733700047929-3fso2o8n83f7dlqo5du9bdep8lphv6n6.apps.googleusercontent.com'
-            onSuccess={ responseGoogle  }
-            onFailure={ responseGoogle  }
-          />
+          <div style={{ margin: '3em' }}>
+            <GoogleSign
+              clientId='733700047929-3fso2o8n83f7dlqo5du9bdep8lphv6n6.apps.googleusercontent.com'
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              buttonText='Login'
+            />
+          </div>
+          <User data={this.state.det} />
         </div>
-
       </div>
     )
   }
--- form/src/User.js
+++ form/src/User.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import GoogleSignOut from 'react-google-login'
+
 
 class User extends Component {
     render() {
-        console.log(this.props.data)
+        // console.log(this.props);
+        const { data } = this.props
+        console.log(data);
+        const user = data ? (
+            <div className='border'>
+                <h4>Profile:</h4>
+                <div className='card-image'>
+                    <img src={data.imageUrl} alt='null' className='circle' />
+                </div>
+                <div className='card-left'>
+                    <h5>User name   : {data.name}</h5>
+                    <h5>First name  : {data.givenName}</h5>
+                    <h5>Last name   : {data.familyName}</h5>
+                    <h5>E-mail      : {data.email}</h5><br />
+                </div>
+            </div>
+        ) : (
+                <div>not yet signed...</div>
+            )
         return (
             <div>
-                
+
+                {user}
             </div>
         )
     }
